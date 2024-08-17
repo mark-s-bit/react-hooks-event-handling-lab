fb3fece8791e2fab4ab1b503f58e24809e381250
mark-s-bit <markchege03@gmail.com>
2024-08-17 23:15:52 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for focus events
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for blur events
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button 
+      onFocus={handleFocus} 
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Event handler for change events
+  const handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <input 
+      type="password" 
+      onChange={handleChange} 
+      placeholder="Enter password" 
+    />
+  );
 }
 
 export default Keypad;
